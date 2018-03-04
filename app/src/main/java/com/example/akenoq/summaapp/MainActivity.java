cfe87aa0a913261a41df3c32b0e4fedb39cfe32a
akenoq <akenoq@yandex.ru>
2018-03-04 21:23:33 +0300

    bug: not works 2 act

--- app/src/main/java/com/example/akenoq/summaapp/MainActivity.java
+++ app/src/main/java/com/example/akenoq/summaapp/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.akenoq.summaapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+
+/*
+    implements View.OnClickListener - для навешивания слушателей
+
+ */
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,18 +21,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void loadArticle (View view) {
+    public void loadArticle(View view) {
+        System.out.print("aaa");
+    }
+
+    public void subscribeArticle(View view) {
 
     }
 
-    public void subscribeArticle (View view) {
-
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_settings:
+                // вызываем вторую активность
+                // создаем обьект intent(контент, класс второй активности)
+                Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                // стартуем вторую активность
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
-
-    public void goToSettingsActivity (View view) {
-
-    }
-
+}
 //    public void find_my_summa (View view) {
 //        EditText t1 = (EditText) findViewById(R.id.ttt_1);
 //        EditText t2 = (EditText) findViewById(R.id.ttt_2);
@@ -37,4 +55,4 @@ public class MainActivity extends AppCompatActivity {
 //        TextView v1 = (TextView) findViewById(R.id.vvv_1);
 //        v1.setText(ans);
 //    }
-}
+
---
+++ app/src/main/java/com/example/akenoq/summaapp/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.example.akenoq.summaapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
